725aba66adf48172ff1297505e722adca21c6e43
Che Ze <admin@josedeza.com>
2020-10-28 21:52:07 +1000

    Worked Out a Method with promise functionality
    To set the data source url inside the confguration object.

--- template/custom_promise_traditional.js
+++ template/custom_promise_traditional.js
@@ -2,46 +2,80 @@ $(function () {
 
     "use strict";
 
-    var sampleFile = false, // "Dev Mode" toggle to prevent making API calls using sample json files instead
-        Geolocation = true; // "Geo Mode" toggle to store browser geolocation coordinates
+    var sampleFile = true, // "Dev Mode" toggle to prevent making API calls using sample json files instead
+        Geolocation = true, // "Geo Mode" toggle to store browser geolocation coordinates
+        configObj = {
+            source: {
+                url: undefined,
+                appId: "393d283150e7d7ced1c524ff318a8870",
+                latitude: 0,
+                longitude: 0,
+                units: "metric", // unit system // metric,imperial
+                exclude: "", // forecast data to exclude // current,minutely,hourly,daily,alert
+
+                // Get wheater data using the following REST API service: api.openweathermap.org
+                // Open Weather Map API Documentation @ https://openweathermap.org/api/one-call-api
+                apiCall: function () {
+                    "use strict";
+
+                    var i = 0,
+                        unitsParameter = "", // unit system // metric,imperial
+                        excludeParameter = "",
+                        url = "";
+
+                    if (this.units) { // Add parameter call only if needed
+                        unitsParameter = "&units=" + this.units;
+                    }
+                    if (this.exclude) { // Add parameter call only if needed
+                        excludeParameter = "&exclude=" + this.exclude;
+                    }
+
+                    url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + this.latitude + "&lon=" + this.longitude + unitsParameter + excludeParameter + "&appid=" + this.appId; // API call
+                    //console.log(url); //DEBUG
+
+                    return url;
+
+                }, // Url to call
+                clientFile: "", // User browser local storage
+                serverFile: "",
+                sampleFile: "/sample_data/openweathermap_brisbane.json",
+            }
+        };
 
 
+    console.log(configObj);
+    console.log("The url is: " + configObj.source.url);
+
+    setSourceUrl(sampleFile, configObj).then(function (obj) {
+        console.log(obj);
+        console.log("The url is: " + obj.source.url);
+    }).catch(function (error) {
+        console.log(error.message);
+    });
 
 
-setDataSource(useSampleFile)
-    .then()
 
 });
 
+//Declare function with promise functionality
+function setSourceUrl(toggleBool, configurationObject) {
+    return new Promise(
+        function (resolve, reject) {
 
+            if (toggleBool) {
+                configurationObject.source.url = configurationObject.source.sampleFile;
+            } else {
+                configurationObject.source.url = configurationObject.source.apiCall();
+            }
 
+            if (configurationObject.source.url) {
+                console.log("fulfilled!");
+                resolve(configurationObject);
+            } else {
+                var reason = new Error('the Url is undefined! man...');
+                reject(reason);
+            }
 
-
-
-
-// call our promise
-function setDataSource(toggleBool,configurationObject) {
-
-  var useSampleFile = new Promise(
-    function (resolve, reject) {
-        if (toggleBool) {
-            configurationObject.source.url = configurationObject.source.sampleFile;
-            resolve(configurationObject);
-        } else {
-            configurationObject.source.url = configurationObject.source.apiCall();
-            reject(configurationObject);
         }
-
-    }
-);
-
-    useSampleFile
-        .then(function (fulfilled) {
-            // yay, you got a new phone
-            console.log(fulfilled);
-        })
-        .catch(function (error) {
-            // ops, mom don't buy it
-            console.log(error.message);
-        });
+    );
 }
